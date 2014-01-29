45c1a63daacc3c6211fa1db433737701711747a7
Lane McIntosh <lanemcintosh@gmail.com>
2014-01-29 10:36:51 -0800

    Added nearest neighbors entropy calculation and added option to not specify minimums and maximums in histogram approach

--- info_theory_functions.py
+++ info_theory_functions.py
@@ -5,9 +5,10 @@ from scipy import *
 
 # in future want to incorporate bias estimators, and add a different way of calculating mutual information without adding and subtracting entropies.
 
-def mutualinfo(x, y, nBins, minX, maxX, minY, maxY):
+def mutualinfo(x, y, nBins, minX=0, maxX=0, minY=0, maxY=0):
     '''Function to compute the mutual information between random variables x and y.  
-    You need to specify the number of bins as well as the minimum and maximum of x and y.
+    You need to specify the number of bins as well as the minimum and maximum of x and y (by default these
+    are just the minimums and maximums of x and y).
     
     x and y also need to be an array, so you could use asarray(x) if you encounter errors.
     
@@ -15,8 +16,14 @@ def mutualinfo(x, y, nBins, minX, maxX, minY, maxY):
     you get artifacts when a random variable moves into another bin whenever you sample.'''
     
     # in future want to make a default for min and max
-    binsX = np.linspace(minX, maxX, nBins+1)
-    binsY = np.linspace(minY, maxY, nBins+1)
+    if minX==maxX:
+        binsX = np.linspace(min(x), max(y), nBins+1)
+    else:
+        binsX = np.linspace(minX, maxX, nBins+1)
+    if minY==maxY:
+        binsY = np.linspace(min(y), max(y), nBins+1)
+    else:
+        binsY = np.linspace(minY, maxY, nBins+1)
     
     # Counting
     CountsXY, xedges, yedges = np.histogram2d(np.squeeze(x),np.squeeze(y), bins=[binsX, binsY])
@@ -69,3 +76,22 @@ def binaryWordsInformation(spikes,stimulus):
     H, I  = mutualinfo(spikes,stimulus,nBins,0,1,min(stimulus),max(stimulus))
     
     return I
+
+
+def nearestNeighborsEntropy(x):
+    '''Compute the binless entropy of a random vector using average nearest 
+    neighbors distance (Kozachenko and Leonenko, 1987).
+
+    '''
+
+    rho = np.zeros((len(x),))
+    H   = 0
+    for idx,pt in enumerate(x):
+        # get second smallest minimum distance (first will always be i=j)
+        rho[idx] = sort(abs(pt - x[:idx]))[1]
+        
+        # H = 1/n sum_i=1^n ln(n*rho_n,i) + ln(2) + Euler's constant
+        H += log((idx+1)*rho[idx])
+    
+
+    return (1.0/float(len(x)))*H + log(2) + 0.5772156649
