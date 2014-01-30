6f909591f4c8c0900b7ce33402458856b0134041
Lane McIntosh <lanemcintosh@gmail.com>
2014-01-30 09:25:14 -0800

    Added simple matlab function to compute nearest neighbors entropy

---
+++ nnEntropy.m
@@ -0,0 +1,15 @@
+function H = nnEntropy(samples)
+%% nnEntropy calculates the nearest neighbors entropy of samples
+%% input is samples by dimensions
+
+k    = size(x,2);
+n    = size(x,1);
+
+Ak = (k*pi^(k/2))/gamma(k/2+1);
+
+% Compute nearest neighbor distances
+distmat = squareform(pdist(samples,'euclidean'));
+distmat = distmat + 1e2*eye(n);
+md      = min(distmat);
+    
+H = k*mean(log2(md)) + log2(n*Ak/k) - psi(1)/log(2);
